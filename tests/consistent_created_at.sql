73bfd9c84bbb82c98ba5664b9b4487ea5ca1e58d
Hussino <hussienalshami@gmail.com>
2024-05-20 13:23:20 +0300

    feature(macro) Added no nulls in columns macro

--- tests/consistent_created_at.sql
+++ tests/consistent_created_at.sql
@@ -1,5 +1,4 @@
-SELECT r.review_date, l.created_at FROM 
-{{ ref('fct_reviews') }} AS r
-RIGHT JOIN
-{{ ref('dim_listings_cleansed') }} AS l
-WHERE r.review_date >= l.created_at
+SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }} r
+USING (listing_id)
+WHERE l.created_at >= r.review_date
